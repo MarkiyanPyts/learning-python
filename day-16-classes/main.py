33bc4be2c0b5a1e63d9eba0310eaeb1c867f58ca
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-08-11 16:09:38 +0300

    work

--- day-16-classes/main.py
+++ day-16-classes/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
 timmy = Turtle()
 timmy.shape('turtle')
@@ -9,3 +10,8 @@ print(timmy)
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
+table = PrettyTable()
+table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('Type', ['Electric', 'Water', 'Fire'])
+table.align = 'l'
+print(table)
