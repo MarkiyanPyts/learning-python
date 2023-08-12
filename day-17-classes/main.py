d496c7c3b411fa79c36cc371477c973054469457
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-08-12 14:16:58 +0300

    Create main.py

---
+++ day-17-classes/main.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self, user_id, user_name):
+        self.id = user_id
+        self.name = user_name
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+    
+user_1 = User('001', 'Mark')
+print(user_1.name)
+
+user_2 = User('002', 'John')
+print(user_2.name)
+
+user_1.follow(user_2)
+
+
+print(user_1.followers)
+print(user_1.following)
+
+print(user_2.followers)
+print(user_2.following)
