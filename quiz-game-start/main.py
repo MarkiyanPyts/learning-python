0456aee6bea123f51cdc11f6ba02ea7a7d931153
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-08-13 08:36:36 +0300

    work

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -11,4 +11,9 @@ for question in question_data:
     questions_bank.append(new_question)
 
 quiz = QuizBrain(questions_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- quiz-game-start/quiz_brain.py
+++ quiz-game-start/quiz_brain.py
@@ -4,7 +4,22 @@ class QuizBrain:
         self.question_list = q_list
         self.score = 0
 
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, connect_answer):
+        if user_answer.lower() == connect_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+
+        print(f'correct answer is {connect_answer}')
+        print(f'your current score is {self.score}/{self.question_number}')
+       
